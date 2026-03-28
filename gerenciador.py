4171cda1eacdb7f7533a165a368177536356d14b
Gustavo Felipe <gustavofcssantos@outlook.com>
2026-03-28 12:24:48 -0300

    Começando Menu do Gerenciador

---
+++ gerenciador.py
@@ -0,0 +1,7 @@
+print("\n Menu do Gerenciador de Lista de Tarefas:")
+print("1. Adicionar tarefa")
+print("2. Ver tarefas")
+print("3. Atualizar tarefa")
+print("4. Completar tarefa")
+print("5. Deletar tarefas completadas")
+print("6. Sair")
