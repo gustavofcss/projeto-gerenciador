b0a56f58e22b955a516cc98b7f8f12c3abfbe207
Gustavo Felipe <gustavofcssantos@outlook.com>
2026-03-28 12:55:11 -0300

    adicionando funcionalidade a escolha 1 e 6

--- gerenciador.py
+++ gerenciador.py
@@ -1,7 +1,26 @@
-print("\n Menu do Gerenciador de Lista de Tarefas:")
-print("1. Adicionar tarefa")
-print("2. Ver tarefas")
-print("3. Atualizar tarefa")
-print("4. Completar tarefa")
-print("5. Deletar tarefas completadas")
-print("6. Sair")
+def adicionar_tarefa(tarefas, nome_tarefa):
+  tarefa = {"tarefa": nome_tarefa, "completada": False}
+  tarefas.append(tarefa)
+  print(f"Tarefa {nome_tarefa} foi adicionada com sucesso!")
+  return
+
+tarefas = []
+while True:
+  print("\n Menu do Gerenciador de Lista de Tarefas:")
+  print("1. Adicionar tarefa")
+  print("2. Ver tarefas")
+  print("3. Atualizar tarefa")
+  print("4. Completar tarefa")
+  print("5. Deletar tarefas completadas")
+  print("6. Sair")
+
+  escolha = input("Digite a sua escolha: ")
+
+  if escolha == "1":
+    nome_tarefa = input("Digite o nome da tarefa que deseja adicionar: ")
+    adicionar_tarefa(tarefas, nome_tarefa)
+  
+  elif escolha == "6":
+    break
+
+print("Programa finalizado")
