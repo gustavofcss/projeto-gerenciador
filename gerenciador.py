5c121d9771c6392de83d2c24bc3570103aa9aceb
Gustavo Felipe <gustavofcssantos@outlook.com>
2026-03-28 13:06:31 -0300

    criando a função ver tarefas para dar funcionalidade a opção 2 do Menu

--- gerenciador.py
+++ gerenciador.py
@@ -4,6 +4,14 @@ def adicionar_tarefa(tarefas, nome_tarefa):
   print(f"Tarefa {nome_tarefa} foi adicionada com sucesso!")
   return
 
+def ver_tarefas(tarefas):
+  print("\n Lista de tarefas:")
+  for indice, tarefa in enumerate(tarefas, start=1):
+    status = "✓" if tarefa["completada"] else " "
+    nome_tarefa = tarefa["tarefa"]
+    print(f"{indice}. [{status}] {nome_tarefa}")
+
+
 tarefas = []
 while True:
   print("\n Menu do Gerenciador de Lista de Tarefas:")
@@ -19,6 +27,9 @@ while True:
   if escolha == "1":
     nome_tarefa = input("Digite o nome da tarefa que deseja adicionar: ")
     adicionar_tarefa(tarefas, nome_tarefa)
+
+  elif escolha == "2":
+    ver_tarefas(tarefas)
   
   elif escolha == "6":
     break
