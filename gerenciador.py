109809d1b359634b4a855b1845542df647b870f7
Gustavo Felipe <gustavofcssantos@outlook.com>
2026-03-30 21:20:18 -0300

    dando funcionalidade as opções 3, 4 e 5

--- gerenciador.py
+++ gerenciador.py
@@ -10,7 +10,29 @@ def ver_tarefas(tarefas):
     status = "✓" if tarefa["completada"] else " "
     nome_tarefa = tarefa["tarefa"]
     print(f"{indice}. [{status}] {nome_tarefa}")
+  return
 
+def atualizar_nome_tarefa(tarefas, indice_tarefa, novo_nome_tarefa):
+  indice_tarefa_ajustado = int(indice_tarefa) - 1
+  if indice_tarefa_ajustado >= 0 and indice_tarefa_ajustado < len(tarefas):
+    tarefas[indice_tarefa]["tarefa"] = novo_nome_tarefa
+    print(f"Tarefa {indice_tarefa} atualizada para {novo_nome_tarefa}")
+  else:
+    print("Índice de tarefa inválido.")
+  return
+
+def completar_tarefa(tarefas, indice_tarefa):
+  indice_tarefa_ajustado = int(indice_tarefa) - 1
+  if indice_tarefa_ajustado >= 0 and indice_tarefa_ajustado < len(tarefas):
+    tarefas[indice_tarefa_ajustado]["completada"] = True
+    print(f"Tarefa {indice_tarefa} marcada como completada")
+  else:
+    print("Índice de tarefa inválido.")
+  return
+
+def deletar_tarefas_completadas(tarefas):
+  print("Tarefas completadas foram deletadas")
+  return
 
 tarefas = []
 while True:
@@ -30,7 +52,22 @@ while True:
 
   elif escolha == "2":
     ver_tarefas(tarefas)
+
+  elif escolha == "3":
+    ver_tarefas(tarefas)
+    indice_tarefa = input("Digite o número da tarefa que deseja atualizar: ")
+    novo_nome= input("Digite o novo nome da tarefa: ")
+    atualizar_nome_tarefa(tarefas, indice_tarefa, novo_nome)
+
+  elif escolha == "4":
+    ver_tarefas(tarefas)
+    indice_tarefa = input("Digite o número da tarefa que deseja completar: ")
+    completar_tarefa(tarefas, indice_tarefa)
   
+  elif escolha == "5":
+    deletar_tarefas_completadas(tarefas)
+    ver_tarefas(tarefas)
+
   elif escolha == "6":
     break
 
